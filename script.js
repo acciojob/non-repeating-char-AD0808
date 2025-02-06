5d4d7228c269e013a0699573a0d2405b657099c6
AD0808 <108204118+AD0808@users.noreply.github.com>
2025-02-06 13:24:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+    let count = 0;
+	let obj = {};
+	
+    
+    for (let i = 0; i < str.length; i++) {
+        if (obj[str[i]] == undefined ) {
+            obj[str[i]] =1;
+        }
+		else{
+			obj[str[i]]++;
+		}
+    }
+
+	for(let x of obj){
+		if(obj[x] ==1)
+		{
+			return x;
+		}
+	}
+    
+	
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
